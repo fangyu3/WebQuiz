1fd5421cee7b5361c8912e9290a7908715909f1d
Tony Fang <tony.fang@cossettemedia.com>
2020-09-01 15:00:11 -0400

    Configured authentication to be stateless

--- src/main/java/com/fangyu3/webquiz/SecurityConfig.java
+++ src/main/java/com/fangyu3/webquiz/SecurityConfig.java
@@ -8,8 +8,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -35,16 +38,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setUserDetailsService(userDetailsService);
         return provider;
     }
+    
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.httpBasic()
-       			.and()
-       		.headers().frameOptions().disable()
-       			.and()
-       		.csrf().disable()
+       http
        		.authorizeRequests()
        			.antMatchers("/api/register").permitAll()
-       			.anyRequest().authenticated();
+       			.anyRequest().authenticated()
+       		.and()
+       		.httpBasic()
+       		.and()
+       		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+       
+  		http.headers().frameOptions().disable();
+  		http.csrf().disable();
+       	
     }
 }
